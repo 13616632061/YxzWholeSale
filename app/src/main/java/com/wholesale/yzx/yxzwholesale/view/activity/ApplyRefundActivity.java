3192162b3cc88128e759e117e1b57ce2938d1219
13530531749@163.com <13530531749@163.com>
2018-08-07 11:39:07 +0800

    订单详情，物流详情，拼单详情

--- app/src/main/java/com/wholesale/yzx/yxzwholesale/view/activity/ApplyRefundActivity.java
+++ app/src/main/java/com/wholesale/yzx/yxzwholesale/view/activity/ApplyRefundActivity.java
@@ -1,6 +1,5 @@
 package com.wholesale.yzx.yxzwholesale.view.activity;
 
-import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -9,7 +8,6 @@ import com.cjj.MaterialRefreshLayout;
 import com.wholesale.yzx.yxzwholesale.R;
 import com.wholesale.yzx.yxzwholesale.base.BaseActivity;
 
-import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class ApplyRefundActivity extends BaseActivity {
